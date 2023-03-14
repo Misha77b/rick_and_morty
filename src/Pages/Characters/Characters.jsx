9f66297d553bba007893e72aee83795e9aa4d0a6
Misha <misha77b@gmail.com>
2023-03-15 00:04:44 +0200

    created loader

--- src/Pages/Characters/Characters.jsx
+++ src/Pages/Characters/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Characters.scss";
 import Logo from "../../components/Logo/Logo";
@@ -6,40 +6,50 @@ import Search from "../../components/Search/Search";
 
 import Card from "../../components/Card/Card";
 import { fetchCharacters } from "../../store/reducers/charactersSlice";
+import Loader from "../../components/Loader/Loader";
 
 const Characters = () => {
   const dispatch = useDispatch();
-  const charactersInfo = useSelector(
-    (state) => state.charactersReducer.charactersInfo
-  );
-  const charactersResults = useSelector(
-    (state) => state.charactersReducer.charactersResults
-  );
+  const [canRender, setCanRender] = useState(undefined);
+
+  const loading = useSelector((state) => state.charactersReducer.loader);
+  const charactersInfo = useSelector((state) => state.charactersReducer.charactersInfo);
+  const charactersResults = useSelector((state) => state.charactersReducer.charactersResults);
 
   console.log(charactersInfo);
   console.log(charactersResults);
 
   useEffect(() => {
+    setCanRender(() => false);
     dispatch(fetchCharacters());
   }, []);
 
+  useEffect(() => {
+    if (!loading && canRender === false) {
+			setCanRender(() => true);
+		}
+  }, [loading]);
+
   return (
     <>
-      <Logo />
-      <Search />
-      <div className="cards-container">
-        {charactersResults.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              name={item.name}
-              species={item.species}
-            />
-          );
-        })}
-      </div>
+      {!canRender ? <Loader /> : <>
+        <Logo />
+        <Search />
+        <div className="cards-container">
+          {charactersResults.map((item) => {
+            return (
+              <Card
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                species={item.species}
+              />
+            );
+          })}
+        </div>
+      </>}
+      
     </>
   );
 };
