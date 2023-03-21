268fc5d1c7d7916a0bd2b45c2a1f4c88dad51efe
Misha <misha77b@gmail.com>
2023-03-21 23:07:43 +0200

    get data from certain location

--- src/store/reducers/locationSlice.js
+++ src/store/reducers/locationSlice.js
@@ -1,15 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-  locationsInfo: {},
-  locationsResults: [],
+  locationsResults: {},
+  locationCharacters: [],
   loader: false,
 };
 
 export const fetchLocations = createAsyncThunk(
-  "characters/fetchData",
-  async () => {
-    const response = await fetch(`https://rickandmortyapi.com/api/location`, {
+  "locations/fetchData",
+  async (id) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/location/${id}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -23,16 +23,40 @@ export const fetchLocations = createAsyncThunk(
   }
 );
 
+export const fetchLocationsCharacters = createAsyncThunk(
+  "locationCharacters/fetchData",
+  async (characters) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${characters}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .catch((err) => {
+        console.warn(err);
+      });
+    return response;
+  }
+);
+
 export const locationsSlice = createSlice({
   name: "locations",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(fetchLocations.pending, (state) => {
+    builder.addCase(fetchLocationsCharacters.pending, (state) => {
       state.loader = true;
     });
     builder.addCase(fetchLocations.fulfilled, (state, action) => {
-      state.locationsInfo = action.payload.info;
-      state.locationsResults = action.payload.results;
+      state.locationsResults = action.payload;
+      //   state.loader = false;
+    });
+    builder.addCase(fetchLocationsCharacters.fulfilled, (state, action) => {
+      //   state.locationCharacters.push(action.payload);
+      state.locationCharacters = action.payload;
       state.loader = false;
     });
   },
